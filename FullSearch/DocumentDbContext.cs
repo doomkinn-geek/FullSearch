5351188112fda89a86b8bb69e0fb763a527a09d3
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-09-20 18:35:07 +0300

    first migration done

---
+++ FullSearch/DocumentDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FullSearch.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullSearch
+{
+    public class DocumentDbContext : DbContext
+    {
+        public virtual DbSet<Document> Documents { get; set; }        
+        public DocumentDbContext(DbContextOptions options) : base(options) { }
+
+    }
+}
--- FullSearch/Program.cs
+++ FullSearch/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace FullSearch
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FullSearch
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services => {
+                    services.AddDbContext<DocumentDbContext>(options =>
+                    {
+                        options.UseSqlServer(@"data source=DESKTOP-FANBABQ\SQLEXPRESS;initial catalog=DocumentsDB;User Id=DocumentsDB;Password=123456;MultipleActiveResultSets=True;App=EntityFramework");
+                    });
+                })
+                .Build();            
         }
     }
 }
