e95ce0c242bfe8799df91cbc411d1a16606fd3f6
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-09-20 19:44:39 +0300

    simple search v3 benchmark

--- FullSearch/Sample2.cs
+++ FullSearch/Sample2.cs
@@ -1,4 +1,6 @@
-﻿using FullSearch.Services.Impl;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using FullSearch.Services.Impl;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,10 +25,33 @@ namespace FullSearch
                 })
                 .Build();
 
-            var documentsSet = DocumentExtractor.DocumentsSet().Take(10000).ToArray();
+            //var documentsSet = DocumentExtractor.DocumentsSet().Take(10000).ToArray();
             //new SimpleSearcher().Search("Monday", documentsSet);
             //new SimpleSearcherV2().SearchV1("Monday", documentsSet);
-            new SimpleSearcherV3().SearchV3("Monday", documentsSet);
+            //new SimpleSearcherV3().SearchV3("Monday", documentsSet);
+
+            BenchmarkSwitcher.FromAssembly(typeof(Sample2).Assembly).Run(args, new BenchmarkDotNet.Configs.DebugInProcessConfig());
+            BenchmarkRunner.Run<SearchBenchmarkV1>();
+        }        
+    }
+
+    [MemoryDiagnoser]
+    [WarmupCount(1)]
+    [IterationCount(5)]
+    public class SearchBenchmarkV1
+    {
+        private readonly string[] _documentsSet;
+
+        public SearchBenchmarkV1()
+        {
+            _documentsSet = DocumentExtractor.DocumentsSet().Take(10000).ToArray();
         }
+
+        [Benchmark]
+        public void SimpleSearch()
+        {
+            new SimpleSearcherV3().SearchV3Enumerable("Monday", _documentsSet).ToArray();
+        }
+
     }
 }
--- FullSearch/Services/Impl/SimpleSearcherV3.cs
+++ FullSearch/Services/Impl/SimpleSearcherV3.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FullSearch.Services.Impl
 {
-    internal class SimpleSearcherV3
+    public class SimpleSearcherV3
     {
         public void SearchV3(string word, IEnumerable<string> data)
         {
@@ -33,6 +33,25 @@ namespace FullSearch.Services.Impl
             var start = Math.Max(0, pos - 50);
             int end = Math.Min(start + 100, text.Length - 1);
             return $"{(start == 0 ? "" : "...")}{text.Substring(start, end - start)}{(end == text.Length - 1 ? "" : "...")}";
+        }
+
+        public IEnumerable<string> SearchV3Enumerable(string word, IEnumerable<string> data)
+        {
+            foreach (var item in data)
+            {
+                int pos = 0;
+                while (true)
+                {
+                    pos = item.IndexOf(word, pos);
+                    if (pos >= 0)
+                    {
+                        yield return PrettyMatchV3(item, pos);
+                    }
+                    else
+                        break;
+                    pos++;
+                }
+            }
         }
     }
 }
